bcd7df82c58de6a1a7f9c1f7afc006c6564cac43
wrafalit@gmail.com <Programowanie2020#>
2022-01-11 20:17:43 +0100

    #improve problem solution

--- Methods and Functions Exercises/1 Function Practice Exercises.py
+++ Methods and Functions Exercises/1 Function Practice Exercises.py
@@ -6,15 +6,10 @@
 
 def lesser_of_two_evens(num1,num2):
     if num1 % 2 == 0 and num2 % 2 ==0:
-        if num1 > num2:
-            return num2
-        else:
-            return num1
+        return min(num1,num2)
     else:
-        if num1 < num2:
-            return num2
-        else:
-            return num1
+        return max(num1,num2)
+
 # Check 1
 print(lesser_of_two_evens(2,4))
 # Check 2
