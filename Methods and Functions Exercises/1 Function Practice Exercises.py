673f0359cc4dab56d3babdf165fe085a3f63df3d
wrafalit@gmail.com <Programowanie2020#>
2022-01-11 20:04:15 +0100

    #making description exc1

---
+++ Methods and Functions Exercises/1 Function Practice Exercises.py
@@ -0,0 +1,5 @@
+# LESSER OF TWO EVENS:
+# Write a function that returns the lesser of two given numbers if both numbers are even,
+# but returns the greater if one or both numbers are odd
+# lesser_of_two_evens(2,4) --> 2
+# lesser_of_two_evens(2,5) --> 5
