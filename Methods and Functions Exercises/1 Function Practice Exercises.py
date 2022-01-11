dabe09311030c9e3f921f5e19d4a8deb4d866daf
wrafalit@gmail.com <Programowanie2020#>
2022-01-11 20:11:07 +0100

    #problem solution

--- Methods and Functions Exercises/1 Function Practice Exercises.py
+++ Methods and Functions Exercises/1 Function Practice Exercises.py
@@ -3,3 +3,19 @@
 # but returns the greater if one or both numbers are odd
 # lesser_of_two_evens(2,4) --> 2
 # lesser_of_two_evens(2,5) --> 5
+
+def lesser_of_two_evens(num1,num2):
+    if num1 % 2 == 0 and num2 % 2 ==0:
+        if num1 > num2:
+            return num2
+        else:
+            return num1
+    else:
+        if num1 < num2:
+            return num2
+        else:
+            return num1
+# Check 1
+print(lesser_of_two_evens(2,4))
+# Check 2
+print(lesser_of_two_evens(2,5))
