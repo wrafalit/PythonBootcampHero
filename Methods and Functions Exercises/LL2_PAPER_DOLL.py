e5cd7a394fdb9104172fdb2d31dbcad8b7a97431
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-13 12:04:50 +0100

    problem solved

--- Methods and Functions Exercises/LL2_PAPER_DOLL.py
+++ Methods and Functions Exercises/LL2_PAPER_DOLL.py
@@ -1,6 +1,15 @@
 
-PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
+# PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 
 # paper_doll('Hello') --> 'HHHeeellllllooo'
 # paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
 
+
+def paper_doll(text):
+    doll_text = ''
+    for ch in text:
+        doll_text= doll_text + ch*3
+    return doll_text
+    
+print(paper_doll('Hello'))
+print(paper_doll('Mississippi')) 
