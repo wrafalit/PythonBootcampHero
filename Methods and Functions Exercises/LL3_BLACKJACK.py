fb3a171e9606bbd4219697f907d8d5c1e92f0a37
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-13 12:20:56 +0100

    Create LL3_BLACKJACK.py

---
+++ Methods and Functions Exercises/LL3_BLACKJACK.py
@@ -0,0 +1,8 @@
+# BLACKJACK: Given three integers between 1 and 11, if their sum is less than or equal to 21, return their sum. 
+# If their sum exceeds 21 and there's an eleven, reduce the total sum by 10. Finally, 
+# if the sum (even after adjustment) exceeds 21, return 'BUST'
+
+# blackjack(5,6,7) --> 18
+# blackjack(9,9,9) --> 'BUST'
+# blackjack(9,9,11) --> 19
+
