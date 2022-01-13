dfdad967b59d14f55eadd8a5a43d4f5a69f11ed8
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-13 10:52:32 +0100

    solve problem

--- Methods and Functions Exercises/LL_1_FIND_33.py
+++ Methods and Functions Exercises/LL_1_FIND_33.py
@@ -3,3 +3,21 @@
 # has_33([1, 3, 3]) → True
 # has_33([1, 3, 1, 3]) → False
 # has_33([3, 1, 3]) → False
+
+
+def has_33(num_list):
+    i = 0
+    while i < (len(num_list) -1):
+        if num_list[i] == 3 and num_list[i] == num_list[i+1]:
+            return True
+        i += 1
+    else:
+        return False
+
+        
+            
+            
+print(has_33([1, 3, 3]))
+print(has_33([1, 3, 1, 3]))
+print(has_33([3, 1, 3]))
+print(has_33([1, 3, 1, 3, 5, 3, 3]))
