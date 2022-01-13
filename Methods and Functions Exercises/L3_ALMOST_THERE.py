0497866eb1c119603b43d1212cb9105e94220cb9
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-13 07:39:05 +0100

    Create L3_ALMOST_THERE.py

---
+++ Methods and Functions Exercises/L3_ALMOST_THERE.py
@@ -0,0 +1,7 @@
+# ALMOST THERE: Given an integer n, return True if n is within 10 of either 100 or 200
+
+# almost_there(90) --> True
+# almost_there(104) --> True
+# almost_there(150) --> False
+# almost_there(209) --> True
+
