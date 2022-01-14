ccdc9c3cc276102a7780c8ece12f96493d6ddd77
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-14 13:09:57 +0100

    Create LLL2_COUNT_PRIMES.py

---
+++ Methods and Functions Exercises/LLL2_COUNT_PRIMES.py
@@ -0,0 +1,7 @@
+# COUNT PRIMES: Write a function that returns the number of prime numbers 
+# that exist up to and including a given number
+
+# count_primes(100) --> 25
+
+# By convention, 0 and 1 are not prime.
+
