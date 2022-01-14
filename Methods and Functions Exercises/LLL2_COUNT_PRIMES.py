277acd916de47e1e45929db9b3370a7eabbea6f9
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-14 15:28:37 +0100

    Update LLL2_COUNT_PRIMES.py

--- Methods and Functions Exercises/LLL2_COUNT_PRIMES.py
+++ Methods and Functions Exercises/LLL2_COUNT_PRIMES.py
@@ -5,3 +5,17 @@
 
 # By convention, 0 and 1 are not prime.
 
+def count_primes(num):
+    count = 0
+    list_primes = list(range(2,num))
+    for n in list(range(2,num)):
+        if n % 1 == 0 and n % n == 0:
+            count += 1
+            
+    print(list_primes)
+    print(count)
+    pass
+                
+
+# Check
+print(count_primes(10))
