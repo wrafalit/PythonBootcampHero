479211540ab580032f6fdaa6dba8c8a782ef166f
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-14 12:47:17 +0100

    problem solved SPY_GAME

--- Methods and Functions Exercises/LLL1_SPY_GAME.py
+++ Methods and Functions Exercises/LLL1_SPY_GAME.py
@@ -4,3 +4,25 @@
 #  spy_game([1,0,2,4,0,5,7]) --> True
 #  spy_game([1,7,2,0,4,5,0]) --> False
 
+def spy_game(nums):
+    add = True
+    bond_list = []
+    for n in nums:
+        if n ==0:
+            bond_list.append(n)
+        elif n==7:
+            bond_list.append(n)
+    if bond_list == [0, 0, 7]:
+        return True
+    else:
+        return False
+            
+
+# Check
+print(spy_game([1,2,4,0,0,7,5]))
+
+# Check
+print(spy_game([1,0,2,4,0,5,7]))
+
+# Check
+print(spy_game([1,7,2,0,4,5,0]))
