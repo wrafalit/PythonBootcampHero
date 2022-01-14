ee5143830bd87c5d70c8c9edf520baf28ee76e20
wrafalit@gmail.com <Programowanie2020#>
2022-01-14 22:32:57 +0100

    # add comments

--- Methods and Functions Exercises/LLL2_COUNT_PRIMES.py
+++ Methods and Functions Exercises/LLL2_COUNT_PRIMES.py
@@ -6,16 +6,13 @@
 # By convention, 0 and 1 are not prime.
 
 def count_primes(num):
-    count = 0
-    i = 0
+    count = 0  # count primes
+    i = 0  # count divisor
     list_primes = list(range(2,num))
     for n in list(range(2,num)):
-        #print(n) check variable
         for nn in list(range(2,n+1)):
-            #print('nn ',nn) check variable
             if n % nn == 0:
                 i +=1
-        #print('i ',i) check variable
         if i == 1:
             count += 1
         i = 0
@@ -25,3 +22,4 @@ def count_primes(num):
 
 # Check
 print(count_primes(100))
+print(count_primes(140))
