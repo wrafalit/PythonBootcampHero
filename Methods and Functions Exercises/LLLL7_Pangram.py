4bb11ab732fe63cfe1a010f5c6704df88a7a72e5
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-18 12:49:12 +0100

    Create LLLL7_Pangram.py

---
+++ Methods and Functions Exercises/LLLL7_Pangram.py
@@ -0,0 +1,24 @@
+# Write a Python function to check whether a string is pangram or not. (Assume the string passed in does not have any punctuation)
+
+# Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
+# For example : "The quick brown fox jumps over the lazy dog"
+
+# Hint: You may want to use .replace() method to get rid of spaces.
+
+# Hint: Look at the string module
+
+# Hint: In case you want to use set comparisons
+
+import string
+
+def ispangram(str1, alphabet=string.ascii_lowercase):
+    pass
+
+ispangram("The quick brown fox jumps over the lazy dog")
+
+# True
+
+string.ascii_lowercase
+
+# 'abcdefghijklmnopqrstuvwxyz'
+
