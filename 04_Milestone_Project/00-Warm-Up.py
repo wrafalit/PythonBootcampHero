fc13ebc9edd1288f4e7bc57c5eecf29f8052f3d3
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-19 14:40:19 +0100

    Update 00-Warm-Up.py

--- 04_Milestone_Project/00-Warm-Up.py
+++ 04_Milestone_Project/00-Warm-Up.py
@@ -1,1 +1,11 @@
 
+def user_choice():
+    x = ''
+    while x.isdigit() == False:
+        x = input('please enter a number: ')
+        if x.isdigit() == False:
+            print('\n'*100)
+            print('Sorry its not a number!')
+    return int(x)
+
+user_choice()
