25f62639aec447b61515d3aa3defc41999c6c6a6
wrafalit <74468704+wrafalit@users.noreply.github.com>
2022-01-19 15:56:28 +0100

    Update Tic_Tac_Toe_Project.py

--- 04_Milestone_Project/Tic_Tac_Toe_Project.py
+++ 04_Milestone_Project/Tic_Tac_Toe_Project.py
@@ -4,3 +4,18 @@
 #     2 players should be able to play the game (both sitting at the same computer)
 #     The board should be printed out every time a player makes a move
 #     You should be able to accept input of the player position and then place a symbol on the board
+
+b1 = 'X'
+b2 = 'X'
+b3 = 'X'
+b4 = 'X'
+b5 = 'X'
+b6 = 'X'
+b7 = 'X'
+b8 = 'X'
+b9 = 'X'
+print(f'{b7} | {b8} | {b9}')
+print('- '+' '+' - '+" "+" - ")
+print(f'{b4} | {b5} | {b6}')
+print('- '+' '+' - '+" "+" - ")
+print(f'{b2} | {b3} | {b4}')
